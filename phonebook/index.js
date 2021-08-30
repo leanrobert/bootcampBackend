07490a0739deb9b21d3dc303597d2790346271a1
leanrobert <53313456+leanrobert@users.noreply.github.com>
2021-08-30 14:00:15 -0300

    part 3.8

--- phonebook/index.js
+++ phonebook/index.js
@@ -4,7 +4,12 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('content', function (req, res) {
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let data = [
     { 
