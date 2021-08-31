9be38d60a2e89a7f07d6ebff44030476fc796ab4
Lean Robert <lean.robert.arg@gmail.com>
2021-08-31 16:35:34 -0300

    heroku

--- phonebook/index.js
+++ phonebook/index.js
@@ -34,8 +34,6 @@ let data = [
     }
 ]
 
-const PORT = 3001
-
 app.get('/api/persons', (req, res) => {
     res.send(data);
 })
@@ -83,6 +81,8 @@ app.get('/info', (req, res) => {
     const date = new Date()
     res.send(`<div><p>Phonebook has info for ${size} people</p><p>${date}</p></div>`)
 })
+
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
