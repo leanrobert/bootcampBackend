0fafbd843bc87a214ba5bd67b0de8b3a091f81da
Lean Robert <lean.robert.arg@gmail.com>
2021-09-05 12:31:59 -0300

    part 3.21

--- phonebook/models/phonebook.js
+++ phonebook/models/phonebook.js
@@ -4,7 +4,7 @@ const uniqueValidator = require('mongoose-unique-validator')
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
-    .then(res => console.log('connected to MongoDB'))
+    .then(res => console.log('connected to MongoDB.'))
     .catch(err => console.log('error connecting to MongoDB:', err.message))
 
 const personSchema = new mongoose.Schema({
